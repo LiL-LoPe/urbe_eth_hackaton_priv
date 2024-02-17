acba2dd896b927acef59c3f773190be289eeca85
LiL-LoPe <117383608+LiL-LoPe@users.noreply.github.com>
2024-02-17 09:24:39 +0100

    lol

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -1,17 +1,14 @@
-// import { HardhatRuntimeEnvironment } from "hardhat/types";
-// import { DeployFunction } from "hardhat-deploy/types";
-// import { Contract } from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { Contract } from "ethers";
 
-// const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-//   const { deployer } = await hre.getNamedAccounts();
-//   const { deploy } = hre.deployments;
-
-//   await deploy("NuminousNecessities", {
-//     from: deployer,
-//     log: true,
-//     autoMine: true,
-//   });
-// };
-
-// export default deployYourContract;
-// deployYourContract.tags = ["NuminousNecessities"];
+const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const { deployer } = await hre.getNamedAccounts();
+    const { deploy } = hre.deployments; await deploy("NuminousNecessities", {
+        from: deployer,
+        log: true,
+        autoMine: true,
+    });
+};
+export default deployYourContract;
+deployYourContract.tags = ["NuminousNecessities"];
