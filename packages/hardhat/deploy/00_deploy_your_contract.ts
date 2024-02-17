3b883577ca472f8b44220603ac91f6e6fada0e2f
OroBianco27 <giucon.mike@outlook.it>
2024-02-17 19:06:27 +0100

    edit robba hardhat

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -1,18 +1,18 @@
-// import { HardhatRuntimeEnvironment } from "hardhat/types";
-// import { DeployFunction } from "hardhat-deploy/types";
-// import { Contract } from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { Contract } from "ethers";
 
-// const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-//   const { deployer } = await hre.getNamedAccounts();
-//   const { deploy } = hre.deployments;
+const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
 
-//   await deploy("NuminousNecessities", {
-//     from: deployer,
-//     args: [],
-//     log: true,
-//     autoMine: true,
-//   });
-// };
+  await deploy("NuminousNecessities", {
+    from: deployer,
+    args: [],
+    log: true,
+    autoMine: true,
+  });
+};
 
-// export default deployYourContract;
-// deployYourContract.tags = ["NuminousNecessities"];
+export default deployYourContract;
+deployYourContract.tags = ["NuminousNecessities"];
